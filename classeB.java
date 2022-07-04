ae44d1c1dcbdd83fddb73de15b0e1e6013f5c321
VictorHuggoMoraes <58454756+VictorHuggoMoraes@users.noreply.github.com>
2022-07-04 16:49:39 -0300

    Add files via upload

---
+++ classeB.java
@@ -0,0 +1,28 @@
+public class classeB {
+    private int B1;
+    private Float B2;
+
+    public int getB1() {
+        return B1;
+    }
+    
+    public void setB1(int b1) {
+        B1 = b1;
+    }
+    
+    public Float getB2() {
+        return B2;
+    }
+    
+    public void setB2(Float b2) {
+        B2 = b2;
+    }
+
+    public void MB1() {
+        System.out.println("MB1");
+    }
+    
+    public void MB2() {
+        System.out.println("MB2");
+    }
+}
